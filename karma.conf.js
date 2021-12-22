27b09abd82fcabad00bf7f2e16750f0e75388946
Louis <1092007@taishinbank.com.tw>
2021-12-22 09:59:52 +0800

    add graphql plugin

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/graphql'),
+      dir: require('path').join(__dirname, './coverage/ng-graphql'),
       subdir: '.',
       reporters: [
         { type: 'html' },
